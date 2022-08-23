f2ca856d99eb1fce0b3af354c721d7b5b8c94e55
Irina Sorokina <irasoro402@yandex.ru>
2022-08-23 19:20:51 +0400

    Added animated

--- lib/tabHome.dart
+++ lib/tabHome.dart
@@ -20,37 +20,34 @@ class _TabHomeWidgetState extends State<TabHomeWidget> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
-          Row(
-            children: [
-              Container(
-                  child: Stack(children: [
-                Opacity(
-                    opacity: 0.5,
-                    child: ClipPath(
-                        clipper: WaveClipper(),
-                        child: Container(
-                          color: Colors.deepPurple.shade200,
-                          width: MediaQuery.of(context).size.width,
-                          height: 200,
-                        ))),
-                ClipPath(
+          Container(
+              child: Stack(children: [
+            Opacity(
+                opacity: 0.5,
+                child: ClipPath(
                     clipper: WaveClipper(),
                     child: Container(
-                      color: Colors.deepPurple.shade300,
+                      color: Colors.deepPurple.shade200,
                       width: MediaQuery.of(context).size.width,
-                      height: 180,
-                      alignment: Alignment.center,
-                      child: Text(
-                        tempLoc.getTotalInf(),
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ))
-              ]))
-            ],
-          ),
+                      height: 200,
+                    ))),
+            ClipPath(
+                clipper: WaveClipper(),
+                child: Container(
+                  color: Colors.deepPurple.shade300,
+                  width: MediaQuery.of(context).size.width,
+                  height: 180,
+                  alignment: Alignment.center,
+                  // child: Text(
+                  //   tempLoc.getTotalInf(),
+                  //   style: TextStyle(
+                  //     fontSize: 18,
+                  //     color: Colors.white,
+                  //   ),
+                  // ),
+                ))
+          ])),
+          const MyStatefulWidget(),
         ],
       ),
     );
@@ -84,4 +81,53 @@ class WaveClipper extends CustomClipper<Path> {
 
   @override
   bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  bool selected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selected = !selected;
+        });
+      },
+      child: Container(
+        child: Align(
+          alignment: Alignment(0.55, 0.0),
+          child: AnimatedContainer(
+            decoration: BoxDecoration(
+              color: selected ? Colors.deepPurple : Colors.deepPurple.shade400,
+              border: Border.all(
+                  width: selected ? 0 : 10, color: Colors.deepPurple.shade200),
+              borderRadius: BorderRadius.circular(100),
+            ),
+            width: selected ? 180.0 : 200.0,
+            height: selected ? 180.0 : 200.0,
+            // color: selected ? Colors.red : Colors.blue,
+            alignment:
+                selected ? Alignment.center : AlignmentDirectional.topCenter,
+            duration: const Duration(seconds: 1),
+            curve: Curves.easeInOutBack,
+            child: Text(
+              tempLoc.getTotalInf(),
+              style: const TextStyle(
+                fontSize: 10,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
