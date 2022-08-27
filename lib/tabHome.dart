9c281ecd7857957b29d7e3f83564702e95f47dc1
Irina Sorokina <irasoro402@yandex.ru>
2022-08-27 15:56:37 +0400

    First version mark of menstruation button

--- lib/tabHome.dart
+++ lib/tabHome.dart
@@ -388,42 +388,77 @@ class _ButtonPeriodWidgetState extends State<ButtonPeriodWidget>
     }
     return Column(
       children: [
+        // OutlinedButton(
+        //   style: OutlinedButton.styleFrom(
+        //     side: const BorderSide(width: 5.0, color: Colors.amber),
+        //   ),
+        //   child: const Text(
+        //     "Mark",
+        //     style: TextStyle(fontSize: 18, color: Colors.amber),
+        //   ),
+        //   onPressed: () {
+        //     _play();
+        //   },
+        // ),
         InfPeriodWidget(),
-        OutlinedButton(
-          style: OutlinedButton.styleFrom(
-            side: const BorderSide(width: 5.0, color: Colors.amber),
-          ),
-          child: const Text(
-            "Mark",
-            style: TextStyle(fontSize: 18, color: Colors.amber),
-          ),
-          onPressed: () {
-            _play();
-          },
-        ),
       ],
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-        animation: animationController,
-        builder: (context, child) {
-          return Container(
-            width: animationSize.value,
-            height: animationSize.value,
-            decoration: BoxDecoration(
-                color: animationColor.value,
-                borderRadius: BorderRadius.circular(120),
-                border: Border.all(
-                  width: 10,
-                  color: animationBorderColor.value,
-                )),
-            child: Center(
-              child: _output(),
+    return Stack(children: [
+      AnimatedBuilder(
+          animation: animationController,
+          builder: (context, child) {
+            return Container(
+              width: animationSize.value,
+              height: animationSize.value,
+              decoration: BoxDecoration(
+                  color: animationColor.value,
+                  borderRadius: BorderRadius.circular(120),
+                  border: Border.all(
+                    width: 10,
+                    color: animationBorderColor.value,
+                  )),
+              child: Center(
+                child: _output(),
+              ),
+            );
+          }),
+      Positioned(
+        bottom: 0,
+        right: 0,
+        child: ElevatedButton(
+          child: Column(children: [
+            Icon(Icons.domain_verification, color: Colors.deepPurple.shade50),
+            const Text(
+              "Mark",
+              style: TextStyle(
+                  fontSize: 10, color: Color.fromRGBO(237, 231, 246, 1)),
             ),
-          );
-        });
+          ]),
+          style: ButtonStyle(
+            side: MaterialStateProperty.all(
+              const BorderSide(
+                color: Color.fromRGBO(179, 157, 219, 1),
+                width: 7,
+              ),
+            ),
+            shape: MaterialStateProperty.all(CircleBorder()),
+            padding: MaterialStateProperty.all(EdgeInsets.all(30)),
+            backgroundColor:
+                MaterialStateProperty.all(Colors.deepPurple.shade400),
+            overlayColor: MaterialStateProperty.resolveWith<Color?>((states) {
+              if (states.contains(MaterialState.pressed))
+                return Colors.deepPurple;
+            }),
+          ),
+          onPressed: () {
+            _play();
+          },
+        ),
+      ),
+    ]);
   }
 }
